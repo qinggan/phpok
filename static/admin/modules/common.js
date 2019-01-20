1341038a7df89fde17ed3887878e18a114072f62
phpok <admin@phpok.com>
2019-01-20 23:16:30 +0800

    更新后台LayUI框架

---
+++ static/admin/modules/common.js
@@ -0,0 +1,13 @@
+/**
+ * 默认LayUI加载的常规项
+ * @作者 qinggan <admin@phpok.com>
+ * @版权 深圳市锟铻科技有限公司
+ * @主页 http://www.phpok.com
+ * @版本 5.x
+ * @授权 http://www.phpok.com/lgpl.html 开源授权协议：GNU Lesser General Public License
+ * @时间 2018年11月25日
+**/
+layui.define(function(e) {
+	var i = (layui.$, layui.layer, layui.laytpl, layui.setter, layui.view, layui.admin);
+	e("common", {});
+});
