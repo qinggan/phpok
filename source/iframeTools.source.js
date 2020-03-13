1bcfc077ce0b7a09ca12aa5e2cbe04bd8947d6c0
phpok <admin@phpok.com>
2020-03-14 03:31:55 +0800

    完善 artDialog 4.x 系列

--- source/iframeTools.source.js
+++ source/iframeTools.source.js
@@ -426,6 +426,50 @@ artDialog.tips = function (content, time, func) {
 	.time(time || 1.5);
 };
 
+artDialog.notice = function (options) {
+    var opt = options || {},
+        api, aConfig, hide, wrap, top,
+        duration = 800;
+        
+    var config = {
+        id: 'Notice',
+        left: '100%',
+        top: '100%',
+        fixed: true,
+        drag: false,
+        resize: false,
+        follow: null,
+        lock: false,
+        init: function(here){
+            api = this;
+            aConfig = api.config;
+            wrap = api.DOM.wrap;
+            top = parseInt(wrap[0].style.top);
+            hide = top + wrap[0].offsetHeight;
+            
+            wrap.css('top', hide + 'px')
+                .animate({top: top + 'px'}, duration, function () {
+                    opt.init && opt.init.call(api, here);
+                });
+        },
+        close: function(here){
+            wrap.animate({top: hide + 'px'}, duration, function () {
+                opt.close && opt.close.call(this, here);
+                aConfig.close = $.noop;
+                api.close();
+            });
+            
+            return false;
+        }
+    };	
+    
+    for (var i in opt) {
+        if (config[i] === undefined) config[i] = opt[i];
+    };
+    
+    return artDialog(config);
+};
+
 
 // 增强artDialog拖拽体验
 // - 防止鼠标落入iframe导致不流畅
