d4dab99a974238e7398124dd07b201d57370afed
phpok <admin@phpok.com>
2022-01-06 19:45:24 +0800

    内置更新包运行（用于生成升级包用）

---
+++ run.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * 升级运行文件
+ * @作者 qinggan <admin@phpok.com>
+ * @主页 http://www.phpok.com
+ * @授权 http://www.phpok.com/lgpl.html 开源授权协议：GNU Lesser General Public License
+ * @时间 2021年12月28日
+**/
+
+/**
+ * 安全限制，防止直接访问
+**/
+if(!defined("PHPOK_SET")){
+	exit("<h1>Access Denied</h1>");
+}
+
+/**
+ * 删除无用的 safecheck
+ * 时间：2021年12月28日
+**/
+$folder = $this->dir_app."safecheck";
+if(file_exists($folder)){
+	$this->lib('file')->rm($this->dir_app."safecheck",true);
+}
+$sql = "SELECT * FROM ".$this->db->prefix."sysmenu WHERE appfile='safecheck'";
+$rs = $this->db->get_one($sql);
+if($rs){
+	$sql = "DELETE FROM ".$this->db->prefix."popedom WHERE gid='".$rs['id']."'";
+	$this->db->query($sql);
+	$sql = "DELETE FROM ".$this->db->prefix."sysmenu WHERE id='".$rs['id']."'";
+	$this->db->query($sql);
+}
+
+/**
+ * 检查财富表字段
+**/
+$fields = $this->db->list_fields('wealth');
+if(!in_array('banner',$fields)){
+	$sql = "ALTER TABLE ".$this->db->prefix."wealth ADD banner varchar(255) NOT NULL COMMENT '大图'";
+	$this->db->query($sql);
+}
+if(!in_array('thumb',$fields)){
+	$sql = "ALTER TABLE ".$this->db->prefix."wealth ADD thumb varchar(255) NOT NULL COMMENT '小图'";
+	$this->db->query($sql);
+}
+if(!in_array('iconfont',$fields)){
+	$sql = "ALTER TABLE ".$this->db->prefix."wealth ADD iconfont varchar(255) NOT NULL COMMENT '字体图标'";
+	$this->db->query($sql);
+}
+
