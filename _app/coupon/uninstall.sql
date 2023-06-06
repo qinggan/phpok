d552c6133364376d50b6ac257a3a53561ba0d231
phpok <admin@phpok.com>
2023-06-06 14:02:31 +0800

    精简应用！一些常用的应用已经集成到云市场

--- _app/coupon/uninstall.sql
+++
@@ -1,7 +0,0 @@
--- 卸载优惠码功能
-
-DROP TABLE IF EXISTS qinggan_coupon;
-
-DROP TABLE IF EXISTS qinggan_coupon_history;
-
-DROP TABLE IF EXISTS qinggan_coupon_user;
--- _app/social/uninstall.sql
+++
@@ -1,4 +0,0 @@
--- 卸载数据库文件，直接在这里写SQL
-
-DROP TABLE IF EXISTS `qinggan_social`;
-DROP TABLE IF EXISTS `qinggan_social_homepage`;
