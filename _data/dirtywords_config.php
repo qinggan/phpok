da41db4d8fa602ccbb61799e28e27940019b98c9
admin <admin@phpok.com>
2023-08-15 10:46:43 +0800

    去除

--- _data/dirtywords_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(!defined("PHPOK_SET")){exit("<h1>Access Denied</h1>");}
-$config["aip_status"] = "0";
-$config["aip_appid"] = "23660086";
-$config["aip_apikey"] = "Z8HGk5zEPfM69eaqg4GVMi0e";
-$config["aip_secret"] = "A4W7qkxOgU80RtNbGCAg59I2SNyPAK9p";
-
-//-----end
